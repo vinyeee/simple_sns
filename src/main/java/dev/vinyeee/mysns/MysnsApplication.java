fbec5b90c9b47f1a8dc5cbb45b1601c274beb0c0
vinyeee <yeevin@naver.com>
2024-11-07 17:35:05 +0900

    init : initialize project

---
+++ src/main/java/dev/vinyeee/mysns/MysnsApplication.java
@@ -0,0 +1,14 @@
+package dev.vinyeee.mysns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class MysnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/vinyeee/mysns/MysnsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vinyeee.mysns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
