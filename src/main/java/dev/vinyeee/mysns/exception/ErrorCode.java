029d6529c7efecf7e1220ee73e8ce55d154c12f1
vinyeee <yeevin@naver.com>
2024-11-15 11:51:08 +0900

    feat: UserService login 로직 exception, 비밀번호 체크 로직 리팩토링  #10

--- src/main/java/dev/vinyeee/mysns/exception/ErrorCode.java
+++ src/main/java/dev/vinyeee/mysns/exception/ErrorCode.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     DUPLICATED_USER_NAME(HttpStatus.CONFLICT,"User name is duplicated"),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"Internal server error");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"Internal server error"),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND,"User not found"),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED,"Password is invalid");
 
 
     private HttpStatus status;
--- src/main/java/dev/vinyeee/mysns/service/UserService.java
+++ src/main/java/dev/vinyeee/mysns/service/UserService.java
@@ -49,11 +49,12 @@ public class UserService {
     public String login(String userName, String password){ // 로그인에 성공하면 jwt 토근 부여
 
         // 회원가입 여부 체크
-        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() -> new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME,""));
+        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND,String.format("%s not found", userName)));
 
         // 비밀번호 체크
-        if (!userEntity.getPassword().equals(password)){
-           throw new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME,"");
+//        if (!userEntity.getPassword().equals(password))
+        if(!encoder.matches(password,userEntity.getPassword())){
+            throw new SnsApplicationException(ErrorCode.INVALID_PASSWORD);
         }
         // 토큰 생성
         return "";
