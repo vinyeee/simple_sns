b0edfaed99a5fa562ce058f10725ba991fc16142
vinyeee <yeevin@naver.com>
2024-12-03 18:30:09 +0900

    feat: LikeRepositoy 생성 및 PostService 에 like 로직 추가

---
+++ simple_sns/src/main/java/com/fastcampus/sns/repository/LikeEntityRepository.java
@@ -0,0 +1,10 @@
+package com.fastcampus.sns.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface LikeRepository extends JpaRepository<LikeRepository, Integer> {
+
+    
+}
